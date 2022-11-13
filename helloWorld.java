106da72ae400b9c0ae8d2b156020fc37fe5045b0
BISHNU1999 <117858232+BISHNU1999@users.noreply.github.com>
2022-11-14 00:20:13 +0530

    Add files via upload
    hello world

---
+++ helloWorld.java
@@ -0,0 +1,20 @@
+
+public class helloWorld {
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String arg[]) {
+		System.out.println("hello world");
+		//println is used to make a new line after the   1st exequiting line
+		
+		System.out.println("hello world");
+		System.out.print("hello world");
+		System.out.print("hello world");
+		// print also can b e used but that time we can not get new line during exeicuiting
+		
+	}
+}
